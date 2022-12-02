b9c243b170c0cf357ce688b726e3e0661e1f3ef8
John Chapman <advent@fussycoder.id.au>
2022-12-02 13:32:01 +1100

    Slight refactor

--- src/day1.rs
+++ src/day1.rs
@@ -23,28 +23,27 @@ pub fn main() {
     part_b(example);
 
     println!("Actual:");
-    part_a(read_to_string("./data/day1a.txt").unwrap().as_str());
-    part_b(read_to_string("./data/day1a.txt").unwrap().as_str());
+    let actual = read_to_string("./data/day1a.txt").unwrap();
+    part_a(actual.as_str());
+    part_b(actual.as_str());
 }
 
-
-pub fn part_a(data: &str) {
+pub fn parse(data: &str) -> Vec<i32> {
     let mut elfs: Vec<i32> = Vec::new();
     for foods in data.split("\n\n") {
         elfs.push(foods.split("\n").map(|x| x.parse::<i32>().unwrap_or(0)).sum());
     }
+    return elfs;
+}
 
+pub fn part_a(data: &str) {
+    let elfs = parse(data);
     let max_calories = *elfs.iter().max().unwrap();
     println!("Elf with max calories: {1} -> {0}", max_calories, elfs.iter().position(|&e| e == max_calories).unwrap()+1);
-
 }
 
 pub fn part_b(data: &str) {
-    let mut elfs: Vec<i32> = Vec::new();
-    for foods in data.split("\n\n") {
-        elfs.push(foods.split("\n").map(|x| x.parse::<i32>().unwrap_or(0)).sum());
-    }
-
+    let elfs = parse(data);
     let mut sorted = elfs;
     sorted.sort();
     println!("Total calories carried by top 3 elves are: {}", sorted[sorted.len()-3..].iter().sum::<i32>());
